4c8c4f20113554d53e218ef0c0a07a0b0d58aede
fgroupindonesia <gumuruh@fgroupindonesia.com>
2020-10-08 06:28:49 +0700

    penambahan class MasakanIkan sesuai homework

---
+++ src/sesi02/MasakanIkan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesi02;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MasakanIkan {
+    int harga;
+    int jumlahQuantity;
+    
+    public void masakPagi(){
+        harga = 3000;
+        jumlahQuantity = 1;
+    }
+    
+    
+    
+}
